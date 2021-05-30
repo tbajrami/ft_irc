60318938af3d4b8fbd088d3bdd2cedf187654c91
Matthieu Thiebaut <mthiebau@z1r10p1.42lyon.fr>
2021-05-30 16:52:57 +0200

    Mise en place ui-server...

--- ui/ui_server/mainwindow.cpp
+++ ui/ui_server/mainwindow.cpp
@@ -7,6 +7,9 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     setWindowTitle("IRC - Server");
+    printLog();
+    updateChannelList();
+    updateClientList();
 }
 
 MainWindow::~MainWindow()
@@ -14,3 +17,38 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::updateClientList()
+{
+    short count = 3;
+//    ui->textClientList->append("NICKNAME");
+//    for (std::map<std::string, Client*>::iterator it = _m_pclients.begin() ; it != _m_pclients.end() ; it++){
+//        ui->textClientList->append(QString::fromStdString(it->second->nickname));
+//        count++;
+//    }
+    ui->NumberClient->display(count);
+//
+    ui->textClientList->append("bob");
+    ui->textClientList->append("albert");
+    ui->textClientList->append("jose");
+//
+}
+
+void MainWindow::updateChannelList()
+{
+    short count = 2;
+//    ui->textChannelList->append("NAME");
+//    for (std::map<std::string, std::string>::iterator it = _m_topics.begin() ; it != _m_topics.end() ; it++){
+//        ui->textChannelList->append(QString::fromStdString(it->first));
+//        count++;
+//    }
+    ui->NumberChannel->display(count);
+//
+    ui->textChannelList->append("#bla");
+    ui->textChannelList->append("#plop");
+//
+}
+
+void MainWindow::printLog()
+{
+    ui->logText->append("LOG SERV !!");
+}
--- ui/ui_server/mainwindow.h
+++ ui/ui_server/mainwindow.h
@@ -14,6 +14,9 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void updateClientList();
+    void updateChannelList();
+    void printLog();
 
 private:
     Ui::MainWindow *ui;
